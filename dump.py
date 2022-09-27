08cdff8fdaf5802ab7746d70076df716bf92c7bd
Helena Rasche <hxr@hx42.org>
2022-09-27 14:06:52 +0200

    add data and update KPIs

---
+++ dump.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import yaml
+import iso3166
+import sys
+
+
+__import__('pprint').pprint(iso3166.countries_by_name)
---
+++ update-kpis.py
@@ -0,0 +1,44 @@
+import glob
+import yaml
+
+with open('kpis.yaml', 'r') as handle:
+    kpis = yaml.safe_load(handle.read())
+
+kpi2 = 0 # events
+kpi3 = 0 # early
+kpi4 = 0 # late
+# kpi5 = 0 # external
+kpi6 = {1:0, 2:0, 3:0, 4:0, 5:0} # quality
+kpi7 = {1:0, 2:0, 3:0, 4:0, 5:0} # satisfaction
+kpi8 = {'Yes':0, 'Maybe':0, 'No': 0} # recommendation
+
+for event in glob.glob("events/*"):
+    kpi2 += 1
+    with open(event, 'r') as handle:
+        data = yaml.safe_load(handle.read())
+        print(data['demographics'])
+        print(data['results'])
+
+        kpi3 += data['demographics']['stage']['early']
+        kpi4 += data['demographics']['stage']['late']
+        #
+        for k, v in data['results']['quality'].items():
+            kpi6[k] += data['results']['quality'][k]
+        for k, v in data['results']['satisfaction'].items():
+            kpi7[k] += data['results']['satisfaction'][k]
+        for k, v in data['results']['recommendation'].items():
+            kpi8[k] += data['results']['recommendation'][k]
+
+kpis['KPI2']['progress'] = kpi2
+kpis['KPI3']['progress'] = kpi3
+kpis['KPI4']['progress'] = kpi4
+
+kpis['KPI6']['progress'] = 2 * sum([k * v for k, v in kpi6.items()]) / sum(kpi6.values())
+kpis['KPI7']['progress'] = 2 * sum([k * v for k, v in kpi7.items()]) / sum(kpi7.values())
+ynm = {'Yes': 5, 'Maybe': 3, 'No': 1}
+kpis['KPI8']['progress'] = 2 * sum([ynm[k] * v for k, v in kpi8.items()]) / sum(kpi8.values())
+
+# kpis['KPI2']['progress'] = kpi2
+
+with open('kpis.yaml', 'w') as handle:
+    yaml.dump(kpis, handle)
