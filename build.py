1af42f71ce40a095f51190475fb590e3c734da2c
Helena Rasche <hexylena@galaxians.org>
2023-10-19 12:46:21 +0200

    update table

--- build.py
+++ build.py
@@ -47,5 +47,64 @@ for k in (6, 7, 8):
             f"success"
         )
 
+# Open the events and pull their data?
+rows = []
+# <th>Title</th>
+# <th>Dates</th>
+# <th>Participants</th>
+# <th>Gender</th>
+# <th>Stage</th>
+# <th>Quality</th>
+# <th>Satisfaction</th>
+# <th>Recommendation</th>
+import glob
+import yaml
+for event in sorted(glob.glob("events/*")):
+    with open(event, 'r') as handle:
+        d = yaml.safe_load(handle)
+        quality = "not reported"
+        if 'results' in d and 'quality' in d['results']:
+            quality = 0
+            for k, v in d['results']['quality'].items():
+                quality += v * k * 2
+            quality_denom = sum(d.get('results', {}).get('quality', {}).values())
+            if quality_denom:
+                quality = round(quality / quality_denom, 1)
+
+        satisfaction = "not reported"
+        if 'results' in d and 'satisfaction' in d['results']:
+            satisfaction = 0
+            for k, v in d['results']['satisfaction'].items():
+                satisfaction += v * k * 2
+            satisfaction_denom = sum(d.get('results', {}).get('satisfaction', {}).values())
+            if satisfaction_denom:
+                satisfaction = round(satisfaction / satisfaction_denom, 1)
+
+        recommendation = "not reported"
+        if 'results' in d and 'recommendation' in d['results']:
+            recommendation = 0
+            for k, v in d['results']['recommendation'].items():
+                recommendation += v if k == "Maybe" or k == "Yes" else 0
+            recommendation_denom = sum(d.get('results', {}).get('recommendation', {}).values())
+            if recommendation_denom:
+                recommendation = round(recommendation * 100 / recommendation_denom, 2)
+
+        q = [
+            d['title'],
+            f"{d['start']} - {d['end']}",
+            sum(d['demographics']['gender'].values()),
+            d.get('demographics', {}).get('gender', {}),
+            d.get('demographics', {}).get('stage', {}),
+            quality,
+            satisfaction,
+            recommendation,
+        ]
+        rows.append(
+                "<tr>" + "".join([f"<td>{x}</td>" for x in q]) + "</tr>")
+tpl = tpl.replace(
+    f'<!-- EVENT_FEEDBACK -->',
+    "\n".join(rows)
+)
+
 with open('index.html', 'w') as handle:
     handle.write(tpl)
